10c073387e94f2b12dba1899ba87e3fdc7f17e38
jedshady <wendelljiang.au@gmail.com>
2017-09-13 19:14:32 +1000

    modify code to enable switch on and off of dropout and bn

--- layers.py
+++ layers.py
@@ -7,31 +7,35 @@ import tensorflow.python.platform
 import tensorflow as tf
 from tensorflow.contrib.layers import xavier_initializer
 
-def conv(input_tensor, name, kw, kh, n_out, dw=1, dh=1, activation_fn=tf.nn.relu):
+def conv(input_tensor, name, kw, kh, n_out, dw=1, dh=1, bn=True, activation_fn=tf.nn.relu):
     n_in = input_tensor.get_shape()[-1].value
     with tf.variable_scope(name):
-        weights = tf.get_variable('weights', [kh, kw, n_in, n_out], tf.float32, xavier_initializer(seed=14))
+        weights = tf.get_variable('weights', [kh, kw, n_in, n_out], tf.float32, xavier_initializer(seed=941))
         biases = tf.get_variable("bias", [n_out], tf.float32, tf.constant_initializer(0.0))
         conv = tf.nn.conv2d(input_tensor, weights, (1, dh, dw, 1), padding='SAME')
-        # batch_mean, batch_var = tf.nn.moments(conv, [0])
-        # conv_bn = tf.nn.batch_normalization(conv, batch_mean, batch_var,
-        #                                 0, 1, variance_epsilon=1e-3)
-        # activation = activation_fn(tf.nn.bias_add(conv_bn, biases))
-        activation = activation_fn(tf.nn.bias_add(conv, biases))
+        if bn:
+            batch_mean, batch_var = tf.nn.moments(conv, [0])
+            conv_bn = tf.nn.batch_normalization(conv, batch_mean, batch_var,
+                                            0, 1, variance_epsilon=1e-3)
+            activation = activation_fn(tf.nn.bias_add(conv_bn, biases))
+        else:
+            activation = activation_fn(tf.nn.bias_add(conv, biases))
         return activation
 
 
-def fully_connected(input_tensor, name, n_out, activation_fn=tf.nn.relu):
+def fully_connected(input_tensor, name, n_out, bn=True, activation_fn=tf.nn.relu):
     n_in = input_tensor.get_shape()[-1].value
     with tf.variable_scope(name):
-        weights = tf.get_variable('weights', [n_in, n_out], tf.float32, xavier_initializer(seed=56))
+        weights = tf.get_variable('weights', [n_in, n_out], tf.float32, xavier_initializer(seed=563))
         biases = tf.get_variable("bias", [n_out], tf.float32, tf.constant_initializer(0.0))
         full_mal = tf.matmul(input_tensor, weights)
-        # batch_mean, batch_var = tf.nn.moments(full_mal, [0])
-        # full_mal_bn = tf.nn.batch_normalization(full_mal, batch_mean, batch_var,
-        #                                     0, 1, variance_epsilon=1e-3)
-        # logits = tf.nn.bias_add(full_mal_bn, biases)
-        logits = tf.nn.bias_add(full_mal, biases)
+        if bn:
+            batch_mean, batch_var = tf.nn.moments(full_mal, [0])
+            full_mal_bn = tf.nn.batch_normalization(full_mal, batch_mean, batch_var,
+                                                0, 1, variance_epsilon=1e-3)
+            logits = tf.nn.bias_add(full_mal_bn, biases)
+        else:
+            logits = tf.nn.bias_add(full_mal, biases)
         return activation_fn(logits)
 
 
@@ -54,6 +58,26 @@ def topK_error(predictions, labels, K=5):
     accuracy = tf.reduce_mean(correct)
     error = 1.0 - accuracy
     return error
+
+def get_vars_name(var_list):
+    var_names = []
+    for elem in var_list:
+        var_names.append(elem.name)
+    return var_names
+
+def get_learning_rate(max_lr, worker_number, epoch, step, steps_per_epoch):
+    incremental = (max_lr - 0.1) / (5 * steps_per_epoch)
+    if epoch < 5:
+        return 0.1 + incremental * ((epoch - 1) * steps_per_epoch + step)
+    elif epoch < 30:
+        return max_lr
+    elif epoch < 60:
+        return max_lr / 10
+    elif epoch < 80:
+        return max_lr / 100
+    else:
+        return max_lr / 1000
+
 
 def average_gradients(grads):
     """Calculate the average gradient for each shared variable across all towers.
--- train_model_simple.py
+++ train_model_simple.py
@@ -22,17 +22,19 @@ config = {}
 
 # customize your model here
 # =========================
-def build_model(input_data_tensor, input_label_tensor, train_mode):
+def build_model(input_data_tensor, input_label_tensor, train_mode, drop_out, batch_norm):
     num_classes = config["num_classes"]
     weight_decay = config["weight_decay"]
 
     # images = tf.image.resize_images(input_data_tensor, [224, 224], method=0, align_corners=False)
     images = input_data_tensor
 
-    logits = vgg.build(images, n_classes=num_classes, training=train_mode)
+    logits = vgg.build(images, n_classes=num_classes, training=train_mode, \
+                        drop_out=drop_out, batch_norm=batch_norm)
     probs = tf.nn.softmax(logits)
     loss_classify = L.loss(logits, tf.one_hot(input_label_tensor, num_classes))
-    loss_weight_decay = tf.reduce_sum(tf.stack([tf.nn.l2_loss(i) for i in tf.get_collection('variables')]))
+    loss_weight_decay = tf.reduce_sum(tf.stack([tf.nn.l2_loss(i) \
+                        for i in tf.get_collection('variables')]))
     loss = loss_classify + weight_decay * loss_weight_decay
     error_top5 = L.topK_error(probs, input_label_tensor, K=5)
     error_top1 = L.topK_error(probs, input_label_tensor, K=1)
@@ -46,19 +48,24 @@ def build_model(input_data_tensor, input_label_tensor, train_mode):
 
 def train(trn_data, tst_data=None):
     learning_rate = config['learning_rate']
+    microbatch_size = config['microbatch_size']
+    num_samples_per_epoch = config["num_samples_per_epoch"]
+    worker_number = config["worker_number"]
+    batch_size = microbatch_size * worker_number
+    steps_per_epoch = num_samples_per_epoch // (worker_number * microbatch_size)
+    max_lr = learning_rate * (worker_number * microbatch_size) / 200
+
     experiment_dir = config['experiment_dir']
     data_dims = config['data_dims']
-    batch_size = config['batch_size']
     num_epochs = config['num_epochs']
-    num_samples_per_epoch = config["num_samples_per_epoch"]
-    steps_per_epoch = num_samples_per_epoch // batch_size
-    num_steps = steps_per_epoch * num_epochs
     checkpoint_dir = pth.join(experiment_dir, 'checkpoints')
     train_log_fpath = pth.join(experiment_dir, 'train.log')
-    vld_iter = config["vld_iter"]
+    # vld_iter = config["vld_iter"]
     checkpoint_iter = config["checkpoint_iter"]
-    worker_number = config["worker_number"]
     pretrained_weights = config.get("pretrained_weights", None)
+    drop_out = config.get("drop_out", True)
+    batch_norm = config.get("batch_normalization", True)
+
 
     # ========================
     # construct training graph
@@ -69,8 +76,10 @@ def train(trn_data, tst_data=None):
         input_label_tensor = tf.placeholder(tf.int32, [None], name='input_label_tensor')
         learning_rate = tf.placeholder(tf.float32, name='lr_placeholder')
         train_mode = tf.placeholder(tf.bool, name='train_mode_placeholder')
-        model = build_model(input_data_tensor, input_label_tensor, train_mode)
+        model = build_model(input_data_tensor, input_label_tensor, \
+                    train_mode, drop_out, batch_norm)
         var_list = tf.trainable_variables()
+        var_name_list = L.get_vars_name(var_list)
         optimizer = tf.train.GradientDescentOptimizer(learning_rate)
         # grads_and_vars = optimizer.compute_gradients(model["loss"])
         grads = tf.gradients(model["loss"], var_list)
@@ -78,7 +87,6 @@ def train(trn_data, tst_data=None):
         aggregated_grads = [tf.placeholder(tf.float32, var.get_shape()) \
                                     for var in var_list]
         grad_step = optimizer.apply_gradients(zip(aggregated_grads, var_list))
-        # init = tf.initialize_all_variables()
         init = tf.global_variables_initializer()
 
 
@@ -88,7 +96,7 @@ def train(trn_data, tst_data=None):
     log = tools.StatLogger(train_log_fpath)
     config_proto = tf.ConfigProto()
     config_proto.gpu_options.allow_growth = True
-    config_proto.gpu_options.per_process_gpu_memory_fraction = 0.4
+    config_proto.gpu_options.per_process_gpu_memory_fraction = 0.3
     sess = tf.Session(graph=G, config = config_proto)
     sess.run(init)
     # tf.train.start_queue_runners(sess=sess)
@@ -99,16 +107,18 @@ def train(trn_data, tst_data=None):
 
         train_x = trn_data[0]
         train_y = trn_data[1]
-        np.random.seed(1234)
+        np.random.seed(983)
         idx = np.arange(num_samples_per_epoch, dtype=np.int32)
 
         for epoch in range(1, num_epochs+1):
-            lr = 0.1 / float(1 << (epoch / 25))
             np.random.shuffle(idx)
             print 'idx = %s' % idx
             total_loss, total_acc, total_acc_5 = 0.0, 0.0, 0.0
             print 'epoch %d' % epoch
             for step in range(steps_per_epoch):
+                # Compute learning rate
+                lr = L.get_learning_rate(max_lr, worker_number, epoch, step, steps_per_epoch)
+
                 X_trn = train_x[idx[step * batch_size: (step + 1) * batch_size]]
                 Y_trn = train_y[idx[step * batch_size: (step + 1) * batch_size]]
 
@@ -119,12 +129,25 @@ def train(trn_data, tst_data=None):
                     micro_x_trn = X_trn[worker * micro_batch_size: (worker + 1) * micro_batch_size]
                     micro_y_trn = Y_trn[worker * micro_batch_size: (worker + 1) * micro_batch_size]
 
-                    ops = [clipped_grads] + [model[k] for k in sorted(model.keys())]
+                    ops = [var_list, clipped_grads] + [model[k] for k in sorted(model.keys())]
                     inputs = {input_data_tensor: micro_x_trn, input_label_tensor: micro_y_trn,
                              learning_rate: lr, train_mode: True}
                     results = sess.run(ops, feed_dict=inputs)
-                    grad_workers.append([grad for grad in results[0]])
-                    results = dict(zip(sorted(model.keys()), results[1:]))
+
+                    ##########################################################
+                    # Print out var and grad norm and the ratio of the norm
+                    ##########################################################
+                    # var_and_grad = zip(results[0], results[1])
+                    # name_and_vg = zip(var_name_list, var_and_grad)
+                    # norm = [[name, np.linalg.norm(np.asarray(value[0])), np.linalg.norm(np.asarray(value[1]))] \
+                    #         for name, value in name_and_vg]
+                    #
+                    # for elem in norm:
+                    #     print '%s, var_norm = %f, grad_norm = %f, ratio = %f' \
+                    #             % (elem[0], elem[1], elem[2], elem[1]/elem[2])
+
+                    grad_workers.append([grad for grad in results[1]])
+                    results = dict(zip(sorted(model.keys()), results[2:]))
                     step_loss += results["loss"]
                     step_acc += 1 - results["error_top1"]
                     step_acc_5 += 1 - results["error_top5"]
@@ -141,6 +164,18 @@ def train(trn_data, tst_data=None):
                 # New Aggregation Strategy:
                 # Scale = exp(1 - v_i / mean(v_i))
                 # new_grad = vr_aggregation(grad_workers)
+                #################################################
+                # Following is for TEST
+                # name_and_gradworkers = zip(var_name_list, grad_workers)
+                # for grad in name_and_gradworkers:
+                #     print grad[0]
+                #     variance = np.var(np.asarray(grad[1]), axis=0)
+                #     avg_variance = np.mean(variance)
+                #     maximum = np.max(variance)
+                #     minimum = np.min(variance)
+                #     info = [avg_variance, maximum, minimum]
+                #     print info
+                # print name_and_gradworkers[1]
 
                 # Old Strategy: Average Aggregation
                 new_grad = avg_aggregation(grad_workers)
@@ -163,8 +198,8 @@ def train(trn_data, tst_data=None):
                 total_acc_5 += avg_step_acc_5
 
                 tools.update_progress(step * 1.0 / steps_per_epoch,
-                            'training loss = %f, accuracy = %f' % (avg_step_loss,
-                                                            avg_step_acc))
+                            'training loss = %f, accuracy = %f, lr = %f' % (avg_step_loss,
+                                                            avg_step_acc, lr))
 
 
                 log.report(epoch=epoch,
@@ -186,7 +221,7 @@ def train(trn_data, tst_data=None):
             inputs = [input_data_tensor, input_label_tensor, train_mode]
             args = [X_tst, Y_tst, False]
             ops = [model[k] for k in sorted(model.keys())]
-            results = tools.iterative_reduce(ops, inputs, args, batch_size=batch_size, fn=lambda x: np.mean(x, axis=0))
+            results = tools.iterative_reduce(ops, inputs, args, batch_size=200, fn=lambda x: np.mean(x, axis=0))
             results = dict(zip(sorted(model.keys()), results))
             print("Test Epoch:%-5d, acc_top1: %.4f, acc_top5: %.4f, loss:%s" % (epoch,
                                                             1-results["error_top1"],
@@ -219,7 +254,7 @@ def avg_aggregation(grad_workers):
     return [np.mean(np.asarray(grad), axis=0) for grad in grad_workers]
 
 def main():
-    batch_size = config['batch_size']
+    # batch_size = config['batch_size']
     experiment_dir = config['experiment_dir']
 
     # setup experiment and checkpoint directories
@@ -230,7 +265,7 @@ def main():
         os.makedirs(checkpoint_dir)
 
     # trn_data_generator, vld_data = dataset.get_cifar10(batch_size)
-    trn_data, tst_data = dataset.get_cifar10(batch_size)
+    trn_data, tst_data = dataset.get_cifar10()
     train(trn_data, tst_data)
 
 
